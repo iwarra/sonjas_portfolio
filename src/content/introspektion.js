cb9be772d60273cd6307c73d8647e12a35d7cb63
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-04-11 13:23:40 +0200

    Poems added

---
+++ src/content/introspektion.js
@@ -0,0 +1,14 @@
+export const poems = [
+	{
+		title: 'Dansa',
+		poem: 'Dansa med mig, <br /> livet spelar sin musik. <br /> Vi får inte välja låtar,<br /> men vi får välja våra steg. <br /><br />Dansa med mig, <br />Följ mig med denna resa. <br />Alla låtar är våra, <br /> även dom vi vill undvika. <br /><br /> Dansa, bara dansa! <br />Med eller utan mig. <br />Livet spelar alltid sin musik, <br /> men vi står på scenen under begränsad tid.',
+	},
+	{
+		title: 'Kärleken vinner',
+		poem: 'Kan vi stanna en stund<br />och tänka på varandra?<br />Medans alla andra springer,<br />medans jorden snurrar runt.<br /><br />Kan vi vara tillsammans<br />i våra tankar<br />tills regnet spolar bort<br />allt som var förut.<br /><br />Kan vi stanna en stund<br />i kärlekens namn?<br />Känner du värmen i hjärtat,<br />Även om jag är inte här?<br /><br />Kan vi vara tillsammans<br />alltid när vi önskar det?<br />Tillåt att i våra tankar<br />kärleken alltid vinner.',
+	},
+	{
+		title: 'Introspektion',
+		poem: 'Inom mig finns oändligheten,<br />en värld skapad av förändringar.<br />Personen som jag var igår<br />kanske inte finns idag?<br /><br />Mörkret och ljuset inom mig<br />kämpar mot varandra konstant…<br />I detta krig vinner ingen…<br />Ett krig utan slut och början.<br /><br />Inom mig gömmer sig mycket…<br />Många ansikten, berättelser och minnen…<br />Många rädslor, drömmar och tvivel…<br />Alla de lever inom mig.<br /><br />Inom mig finns en hel värld<br />som även jag knappt kan se.<br />Om jag inte känner igen denna värld?<br />Vem kan då veta något mer?<br /><br />',
+	},
+];
