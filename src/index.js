bdc5374b796f2db650e06cec918764fdb4063e8e
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-04-11 13:22:53 +0200

    New routes for about and news / scroll to top update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './views/HomePage.vue';
 import IntrospectionPage from './views/IntrospectionPage.vue';
+import AboutPage from './views/AboutPage.vue';
+import NewsPage from './views/NewsPage.vue';
 
 const routes = [
 	{ path: '/', component: HomePage },
@@ -8,9 +10,15 @@ const routes = [
 		path: '/introspektion',
 		component: IntrospectionPage,
 	},
+	{ path: '/om', component: AboutPage },
+	{ path: '/nyheter', component: NewsPage },
 ];
 
 const router = createRouter({
+	scrollBehavior(to, from, savedPosition) {
+		// always scroll to top
+		return { top: 0 };
+	},
 	history: createWebHistory(),
 	routes,
 });
---
+++ src/views/NewsPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>
+		<h1>Inkommande</h1>
+	</div>
+</template>
+
+<style scoped></style>
