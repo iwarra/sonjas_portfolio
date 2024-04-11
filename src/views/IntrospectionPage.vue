f423422c4e40820f704bc367b3a273f801d36bb5
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-04-11 13:25:03 +0200

    Started page about the book

--- src/views/IntrospectionPage.vue
+++ src/views/IntrospectionPage.vue
@@ -1,20 +1,78 @@
+<script setup>
+import { poems } from '../content/introspektion.js';
+</script>
+
 <template>
 	<div class="wrapper">
-		<h1>Introspektion</h1>
-		<img
-			src="/src/assets/introspektion_bok.jpg"
-			alt="" />
+		<h1 class="title">Introspektion</h1>
+		<div class="intro">
+			<img
+				src="/src/assets/introspektion_bok.jpg"
+				alt="" />
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam sapiente harum corrupti. Ut
+				minus in, repudiandae, culpa aliquid porro voluptate rem ab magnam, est at ad iusto quisquam
+				reprehenderit enim.
+			</p>
+		</div>
 	</div>
+	<ul style="list-style: none">
+		<li
+			v-for="poem in poems"
+			:key="poem.title">
+			<h2>
+				{{ poem.title }}
+			</h2>
+			<p v-html="poem.poem"></p>
+		</li>
+	</ul>
 </template>
-
-<script setup></script>
 
 <style scoped>
 .wrapper {
-	min-width: 80vw;
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+}
+.title {
+	overflow: hidden;
+	white-space: nowrap;
+	border-right: 2px solid transparent;
+	width: 0;
+	max-width: max-content;
+	animation: typing 2s steps(30, end) forwards, blinking 2.5s 1;
 }
 
-img {
-	max-height: 400px;
+@keyframes typing {
+	from {
+		width: 0;
+	}
+	to {
+		width: 100%;
+	}
+}
+
+@keyframes blinking {
+	0% {
+		border-color: transparent;
+	}
+	50% {
+		border-color: black;
+	}
+	100% {
+		border-color: transparent;
+	}
+}
+
+.intro {
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+	flex-wrap: wrap;
+	flex: 0 1 40%;
+
+	img {
+		max-height: fit-content;
+	}
 }
 </style>
