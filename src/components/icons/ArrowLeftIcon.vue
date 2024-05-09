797db2474e9d1131185e771feec8953436a526e7
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-05-09 11:02:20 +0200

    Updated color and size for arrow icons

--- src/components/icons/ArrowLeftIcon.vue
+++ src/components/icons/ArrowLeftIcon.vue
@@ -1,11 +1,11 @@
 <template>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		width="1.5em"
-		height="1.5em"
+		width="1.9em"
+		height="1.9em"
 		viewBox="0 0 26 26">
 		<path
-			fill="#888888"
+			fill="#be688e"
 			d="M10 22L0 12L10 2l1.775 1.775L3.55 12l8.225 8.225z"></path>
 	</svg>
 </template>
--- src/components/icons/ArrowRightIcon.vue
+++ src/components/icons/ArrowRightIcon.vue
@@ -1,11 +1,11 @@
 <template>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		width="1.5em"
-		height="1.5em"
+		width="1.9em"
+		height="1.9em"
 		viewBox="0 0 24 24">
 		<path
-			fill="#888888"
+			fill="#be688e"
 			d="M8.025 22L6.25 20.225L14.475 12L6.25 3.775L8.025 2l10 10z"></path>
 	</svg>
 </template>
