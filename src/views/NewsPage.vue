79f5d35440cf091c6d86020a701a926556277f3b
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-05-16 22:16:41 +0200

    Logic for getting blog posts

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -1,10 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+const posts = ref();
+const getJsonFiles = import.meta.glob('../blog/_posts/*.json', { eager: true });
+
+posts.value = Object.entries(getJsonFiles).map((entry) => {
+	const url = entry[0].replace('../blog/_posts/', '').replace('.json', '');
+	const title = entry[1].title;
+	const data = entry[1].default;
+	const slug = entry[1].summary;
+
+	return {
+		...data,
+		slug,
+		url,
+		title,
+	};
+});
+</script>
 
 <template>
 	<div>
-		<h1>
-			<span class="accent">Kommande</span>
-		</h1>
+		<div>
+			<h1>Nyheter</h1>
+			<div
+				v-for="post in posts"
+				:key="post.title">
+				<RouterLink :to="`/nyheter/${post.url}`">
+					<h2>{{ post.title }}</h2>
+				</RouterLink>
+				<span>{{ post.summary }}</span>
+			</div>
+		</div>
 	</div>
 </template>
 
