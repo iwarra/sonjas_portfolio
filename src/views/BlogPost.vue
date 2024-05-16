5e85a27e02ae673b9493cd3a2b9fdce8cdf2954f
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-05-16 22:17:44 +0200

    Logic to fetch a single post via GitHub API

---
+++ src/views/BlogPost.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<h1>{{ post.title }}</h1>
+		<span>Publicerad: {{ post.date.slice(0, 10) }}</span>
+		<p>{{ post.body }}</p>
+	</div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+import { Octokit } from 'https://esm.sh/octokit';
+import { ref } from 'vue';
+
+const route = useRoute();
+const postTitle = route.params.title + '.json';
+const post = ref();
+
+const octokit = new Octokit({
+	auth: import.meta.env.API_TOKEN,
+});
+
+async function getFile(owner, repo, path) {
+	try {
+		const response = await octokit.request(`GET /repos/{owner}/{repo}/contents/{path}`, {
+			owner,
+			repo,
+			path,
+		});
+		// Decode the content from base64
+		const content = atob(response.data.content);
+		return content;
+	} catch (error) {
+		console.error('Error fetching file:', error);
+		return null;
+	}
+}
+
+getFile('iwarra', 'sonjas_portfolio', `src/blog/_posts/${postTitle}`).then((content) => {
+	post.value = JSON.parse(content);
+	console.log(post.value);
+});
+</script>
+
+<style scoped></style>
